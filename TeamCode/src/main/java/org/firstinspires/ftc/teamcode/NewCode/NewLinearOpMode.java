4ef2a62de4d7e0e860c55522e74e5dfd0f472d4e
Nick Fanell <manelliproductions@gmail.com>
2018-12-15 09:49:14 -0500

    Made Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewCode/NewLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewCode/NewLinearOpMode.java
@@ -39,22 +39,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name="New: Linear OpMode", group="New")
 
 public class NewLinearOpMode extends LinearOpMode {
+
+    enum Box {
+        LEFT, RIGHT
+    }
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -70,8 +62,12 @@ public class NewLinearOpMode extends LinearOpMode {
     private DcMotor swing = null;
 
     private Servo boot;
+    private Servo boxLeft;
+    private Servo boxRight;
 
     private boolean reverse;
+    private boolean boxLeftPositon = false;
+    private boolean boxRightPosition = false;
 
     @Override
     public void runOpMode() {
@@ -90,6 +86,9 @@ public class NewLinearOpMode extends LinearOpMode {
         lift = hardwareMap.dcMotor.get("lift");
         swing = hardwareMap.dcMotor.get("swing");
 
+        boxLeft = hardwareMap.servo.get("boxleft");
+        boxRight = hardwareMap.servo.get("boxright");
+
         intake = hardwareMap.dcMotor.get("intake");
 
         boot = hardwareMap.servo.get("boot");
@@ -106,38 +105,27 @@ public class NewLinearOpMode extends LinearOpMode {
         fr.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.REVERSE);
 
+        //Init servos
         boot.setPosition(0);
+        boxLeft.setPosition(0);
+        boxRight.setPosition(0);
 
-        // Wait for the game to start (driver presses PLAY)
+
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
-//            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-//            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
             //Call the conrollers
             controller1(gamepad1);
             controller2(gamepad2);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-
-            // Send calculated power to wheels
-
+            //Check to see if the robot is in reverse
             if(!reverse) {
                 leftPower = -gamepad1.left_stick_y;
                 rightPower = -gamepad1.right_stick_y;
@@ -151,7 +139,6 @@ public class NewLinearOpMode extends LinearOpMode {
             br.setPower(rightPower);
             fr.setPower(rightPower);
 
-            // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Lift Encoder", lift.getCurrentPosition());
             telemetry.addData("Swing Encoder", swing.getCurrentPosition());
@@ -229,11 +216,32 @@ public class NewLinearOpMode extends LinearOpMode {
             swing.setPower(0);
         }
         //==========================================================================================
-        if(gamepad.y) {
-           boot.setPosition(1);
+        if(gamepad.right_bumper) {
+            toggleBox(Box.RIGHT);
         }
-        if(gamepad.a) {
-            boot.setPosition(0);
+        //==========================================================================================
+        if(gamepad.left_bumper) {
+            toggleBox(Box.LEFT);
+        }
+    }
+
+    private void toggleBox(Box box) {
+        if(box == Box.LEFT) {
+            if(!boxLeftPositon) {
+                boxLeft.setPosition(1);
+                boxLeftPositon = true;
+            } else {
+                boxLeft.setPosition(0);
+                boxLeftPositon = false;
+            }
+        } else {
+            if(!boxRightPosition) {
+                boxRight.setPosition(1);
+                boxRightPosition = true;
+            } else {
+                boxRight.setPosition(0);
+                boxRightPosition = false;
+            }
         }
     }
 }
